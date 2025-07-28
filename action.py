178dadb8e20f02aa13c2b2607d1ac398f3a6473f
Sky <ppk521@york.ac.uk>
2025-07-28 10:00:16 +0100

    Working on getting mainsys to run obj detection in paralell to interacting with the LEGO hub.

--- action.py
+++ action.py
@@ -1,12 +1,36 @@
-
+from pyboard import Pyboard
 
 class Act():
     def __init__(self):
+        self.baudrate = 115200 # This is the standard board rate to communicate with the Lego Hub
+        self.device = '/dev/ttyACM0' 
+        self.wait= 0
+        self.pyb = Pyboard(self.device, self.baudrate, self.wait)
+        self.pyb.enter_raw_repl()
         return
     
     def add_to_parser(self, parser):
         parser.add_argument('--actiondir', help='Specify act/movement control file', default=None)
         
-    def run(self):
-        #print("Action function")
+    async def run(self):
+        try:
+            while True:
+                say_text_wait(pyb, 'Starting')
+                await asyncio.sleep(1)
+        
+        # Only terminate when program is to be halted.
+        finally:
+            self.pyb.exit_raw_repl()
+            self.pyb.close()
         return
+    
+    def say_text_wait(pyb, sentence):
+        command = f"""\
+from hub import light_matrix
+import runloop
+async def main():
+    await light_matrix.write('{sentence}')
+
+runloop.run(main())
+"""
+        pyb.exec(command)
